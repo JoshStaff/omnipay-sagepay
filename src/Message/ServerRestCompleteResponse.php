3238c679acef45722494e07655d2938b6b464cda
Adam Auskerin <adam.auskerin@gmail.com>
2022-01-12 15:28:35 +0000

    Fix interpretation of 3DS statuses
    The previous implementation of the ServerRestCompleteResponse would have
    suggested that a transaction was fully authorised if the 3DS status was
    authenticated even if the transaction itself was declined by the bank,
    because the transaction status was only checked as a fallback if the 3DS
    status was not authenticated.
    
    Now, both the transaction and 3DS statuses must be "successful".

--- src/Message/ServerRestCompleteResponse.php
+++ src/Message/ServerRestCompleteResponse.php
@@ -8,12 +8,24 @@ namespace Omnipay\Opayo\Message;
 class ServerRestCompleteResponse extends RestResponse
 {
     /**
+     * For 3DSv2, this returns true only if the transaction was authorised by the card issuer and the 3DSecure status is
+     * one that could generally be considered safe to proceed. Depending on how much liability you wish to accept, you
+     * might prefer to explicitly check the 3DSecure status and handle some of them differently.
+     *
+     * For the 3DSv1 fallback, a true result here only means that the cardholder successfully authenticated. You will
+     * need to perform a separate request to retrieve the transaction and confirm that it was actually authorised by the
+     * card issuer.
      *
      * @return bool
      */
     public function isSuccessful()
     {
-        return $this->get3DSecureStatus() === static::REST_3DSECURE_STATUS_AUTHENTICATED
-            || $this->getStatus() === static::OPAYO_STATUS_AUTHENTICATED;
+        return parent::isSuccessful() && in_array($this->get3DSecureStatus(), [
+            null,
+            static::REST_3DSECURE_STATUS_AUTHENTICATED,
+            static::REST_3DSECURE_STATUS_NOT_CHECKED,
+            static::REST_3DSECURE_STATUS_CARD_NOT_ENROLLED,
+            static::REST_3DSECURE_STATUS_ISSUER_NOT_ENROLLED,
+        ]);
     }
 }
